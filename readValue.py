e0c5d957ce4fa921c9acc2a0c1056581fe4fab8b
thuanntd2001 <thiensudanduong12@gmail.com>
2023-03-27 10:17:21 +0700

    xong doc tai liru

--- readValue.py
+++ readValue.py
@@ -1,6 +1,9 @@
 # doc lst tu vung
 f = open("npl/term-vocab", "r")
 lstVocab = []
+lstDoc = [""] * 12000
+lstQuery = [""] * 100
+
 
 for i in f:
     record = i.split()
@@ -9,24 +12,67 @@ for i in f:
         lstVocab.append(record[1])
     except Exception:
         break
+f.close()
+
+
 # doc lst van ban
 f = open("npl/doc-text", "r")
-lstDoc = [""]*8000
 iterFile = iter(f)
-strI = next(iterFile)
-while strI:
+
+strI = ""
+while True:
 
     try:
+        strI = next(iterFile)
         i = int(strI)
-        print("doc i= ",i )
+        print("doc i= ", i)
     except ValueError:
+        print("format tai lieu ", i)
         break
+    except StopIteration as e:
+        print(e)
+        break
+
     strI = next(iterFile)
 
-    while strI != "   /":
+    while "/" not in strI:
         lstDoc[i] += strI
         print(strI)
         strI = next(iterFile)
     print("xong doc", i)
 
+#print(lstDoc[1])
+f.close()
 
+
+
+
+# doc lst query
+
+f = open("npl/query-text", "r")
+iterFile = iter(f)
+
+strI = ""
+while True:
+
+    try:
+        strI = next(iterFile)
+        i = int(strI)
+        print("query i= ", i)
+    except ValueError:
+        print("format tai lieu ", i)
+        break
+    except StopIteration as e:
+        print(e)
+        break
+
+    strI = next(iterFile)
+
+    while "/" not in strI:
+        lstQuery[i] += strI
+        print(strI)
+        strI = next(iterFile)
+    print("xong query", i)
+
+#print(lstQuery[1])
+f.close()
